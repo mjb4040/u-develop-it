914c2d493820fd244af003d92e3c887588d6bc90
mjb4040 <michaeljamesblackwell.@gmail.com>
2021-11-12 12:56:44 -0500

    db query *

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'password',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
 app.get('/', (req, res) => {
     res.json({
@@ -15,7 +29,11 @@ app.get('/', (req, res) => {
 app.use((req, res) => {
     res.status(404).end();
   });
-  
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
